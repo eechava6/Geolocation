91a092e0cd149868b45a6c4725bbf6f9b0f0a7be
eechava6 <eechava6@eafit.edu.co>
2019-01-28 15:48:28 -0500

    Fixed redundant body-parser

--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../app/api/controllers/users');
-const bodyParser = require('body-parser');
-
-router.use(bodyParser.json());
-router.use(bodyParser.urlencoded({extended: false}));
 
 router.post('/register', userController.create);
 router.post('/authenticate', userController.authenticate);
-
-
-
-
-
 
 module.exports = router;

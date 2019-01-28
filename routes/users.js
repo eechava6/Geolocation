59c6b4bda09757980fcc699d8f99b97907d6596b
eechava6 <eechava6@eafit.edu.co>
2019-01-28 17:15:11 -0500

    Improved URI names for actions

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../app/api/controllers/users');
 
-router.post('/register', userController.create);
-router.post('/authenticate', userController.authenticate);
+router.post('/registerUser', userController.create);
+router.post('/authenticateUser', userController.authenticate);
 
 module.exports = router;

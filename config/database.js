3b17410060aa4753348b79c8933e815d49434753
eechava6 <eechava6@eafit.edu.co>
2019-02-03 17:08:36 -0500

    updated DB config

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 //Set up mongoose connection and db config
 const mongoose = require('mongoose');
-const mongoDB = 'mongodb://localhost:27017/trackingGPS';
+const mongoDB = 'mongodb://mongo-server:27017/trackingGPS';
 mongoose.connect(mongoDB,  { useNewUrlParser: true });
 mongoose.Promise = global.Promise;
 var db = mongoose.connection;

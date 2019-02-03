c2e5046a528971c6ae9fdf5c979853d39ec2f03f
eechava6 <eechava6@eafit.edu.co>
2019-02-03 12:30:16 -0500

    developing branch

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 //Set up mongoose connection
 const mongoose = require('mongoose');
-const mongoDB = 'mongodb://www.eechava6.tk/trackingGPS';
+const mongoDB = 'mongodb://localhost:27017/trackingGPS';
 mongoose.connect(mongoDB,  { useNewUrlParser: true });
 mongoose.Promise = global.Promise;
 var db = mongoose.connection;

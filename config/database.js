31399bb93064d9db11dc0b7351d224fcf725f2de
eechava6 <eechava6@eafit.edu.co>
2019-02-09 12:20:23 -0500

    Updated DB conf, using NODE_ENV

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 //Set up mongoose connection and db config
-const mongoose = require('mongoose');
+/*const mongoose = require('mongoose');
 const mongoDB = 'mongodb://localhost:27017/trackingGPS';
 mongoose.connect(mongoDB,  { useNewUrlParser: true });
 mongoose.Promise = global.Promise;
@@ -8,3 +8,42 @@ db.once('open', function() {
     console.log("Connected to DB")
     });
 module.exports = mongoose;
+*/
+
+var path = require('path'),
+    rootPath = path.normalize(__dirname + '/..'),
+    env =  process.env.NODE_ENV || 'development';   
+
+var config = {
+  dev: {
+    baseUrl: "/",
+    root: rootPath,
+    app: {
+      name: 'TrackingGpsPR1'
+    },
+    port: process.env.PORT || 3000,
+    db: 'mongodb://localhost/TrackingGps'
+  },
+
+  test: {
+    baseUrl: "/",
+    root: rootPath,
+    app: {
+      name: 'TrackingGpsPR1'
+    },
+    port: process.env.PORT || 3000,
+    db: 'mongodb://mongo-server/TrackingGps'
+  },
+
+  production: {
+    baseUrl: "/",
+    root: rootPath,
+    app: {
+      name: 'TrackingGpsPR1'
+    },
+    port: process.env.PORT || 3000,
+    db: 'mongodb://mongo-server/TrackingGps'
+  }
+};
+
+module.exports = config[env];
